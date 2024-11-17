9684d2ab40220a68182c89af5d0b183c628ecd40
Ryan Salsbury <ryanrs@gmail.com>
2024-11-17 14:08:32 -0800

    Refactor for clarity
    Signed-off-by: Ryan Salsbury <ryanrs@gmail.com>

--- lua/src/temperature.lua
+++ lua/src/temperature.lua
@@ -1,14 +1,13 @@
 local function publish_temperature ()
     while true do
-        local temp = read_temperature()
-        if temp == nil then
-            -- error reading temp sensor
-            return
-        end
-        local data = { temperature = temp }
+        local data = { temperature = read_temperature() }
         MQ.Publish("usb-io/temperature", json.encode(data))
         coroutine.yield(15 * 1000)
     end
 end
 
-scheduler.start(coroutine.create(publish_temperature))
+if read_temperature() == nil then
+    print("Error: temperature sensor not present.")
+else
+    scheduler.start(coroutine.create(publish_temperature))
+end
