d6dbacfad3c108872f070dda06937b07ad6c0c2c
Ryan Salsbury <ryanrs@gmail.com>
2024-11-17 16:04:41 -0800

    Scheduler comments
    Signed-off-by: Ryan Salsbury <ryanrs@gmail.com>

--- lua/src/scheduler.lua
+++ lua/src/scheduler.lua
@@ -11,9 +11,13 @@ scheduler.pq = PriorityQueue{
 
 scheduler.interrupts = {}
 
+-- Called by the main Arduino loop.
+-- The C code looks up "scheduler.loop" in the Lua global scope and calls it.
+-- Returns the number of millseconds the system should sleep.
 function scheduler.loop (ints)
     scheduler.recent_ms = time.millis()
 
+    -- If we have pending interrupts, wake any threads waiting on them.
     if ints ~= 0 then
         for co, co_ints in pairs(scheduler.interrupts) do
             if ints & co_ints ~= 0 then
@@ -22,34 +26,49 @@ function scheduler.loop (ints)
             end
         end
     end
+    -- TODO: save interrupt bits that nobody is waiting on so they
+    -- don't get lost. Needed once my interrupt handlers
+    -- get more complex.
 
+    -- Now run any threads that are ready.
     while not scheduler.pq:empty() do
+        -- Find next thread to run according to wakeup time.
         local co, t = scheduler.pq:peek()
         local ms = time.millis()
         if t - ms > 0 then
+            -- no threads ready to run
             return t - ms
         end
 
         co = scheduler.pq:dequeue()
         scheduler.interrupts[co] = nil
 
+        -- Run thread coroutine.
         local r, t_inc, co_ints = coroutine.resume(co, ms)
         if r and t_inc then
+            -- coroutine wants to sleep
             scheduler.pq:enqueue(co, time.millis() + math.floor(t_inc))
             if co_ints and co_ints > 0 then
+                -- coroutine is listening for interrupts
                 scheduler.interrupts[co] = co_ints
             end
         else
+            -- coroutine has exited
             coroutine.close(co)
         end
     end
+    -- All threads have exited, so just sleep for a second.
+    -- We'll be called early if a new thread is created.
     return 1000
 end
 
+-- Start a new thread after t_inc milliseconds.
 function scheduler.start (co, t_inc)
     scheduler.pq:enqueue(co, time.millis() + math.floor(t_inc or 0))
 end
 
+-- Reschedule a thread to run after t_inc milliseconds.
+-- Can be used to wake up early.
 function scheduler.wake (co, t_inc)
     scheduler.pq:update(co, time.millis() + math.floor(t_inc or 0))
 end
