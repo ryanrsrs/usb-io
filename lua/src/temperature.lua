826ecd8b38854d4fbc07b35ce1b0dcbbab3fa0d6
Ryan Salsbury <ryanrs@gmail.com>
2024-11-20 14:51:22 -0800

    Temperature: print less often
    Signed-off-by: Ryan Salsbury <ryanrs@gmail.com>

--- lua/src/temperature.lua
+++ lua/src/temperature.lua
@@ -2,7 +2,7 @@ local function publish_temperature ()
     while true do
         local data = { temperature = read_temperature() }
         MQ.Publish("usb-io/temperature", json.encode(data))
-        coroutine.yield(15 * 1000)
+        coroutine.yield(60 * 1000)
     end
 end
 
